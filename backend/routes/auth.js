aa5257a417e02a02bb9786ac0d45f8cfca75feab
小野瑛人 <eito@Eitos-MacBook-Air.local>
2025-01-29 17:32:18 +0900

    ユーザー情報のスキーマ作成、ユーザーの登録APIとユーザーのログインAPIの作成

---
+++ backend/routes/auth.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const User = require('../models/UserSchema');
+
+//user registration API
+router.post('/register', async (req, res) => {
+    try {
+        const newUser = await new User({
+            username: req.body.username,
+            password: req.body.password,
+        });
+        const user = await newUser.save();
+        res.status(200).json(user);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//user login API
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username }); //usernameで対象ユーザーを検索
+        if (!user) { res.status(400).json("User not found"); }
+
+        const validPassword = user.password === req.body.password; //パスワードの照合
+        if (!validPassword) { res.status(400).json("Wrong password"); }
+
+        res.status(200).json(user);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
