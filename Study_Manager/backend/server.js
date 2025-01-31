7aec6a54c7d48b49d2bfac8bac735c806e231a20
bp22083(mitobe kaito) <bp22083@sic.shibaura-it.ac.jp>
2025-01-31 22:05:07 +0900

    .envファイルを追加

--- Study_Manager/backend/server.js
+++ Study_Manager/backend/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-// const userRouter = require('./routes/users');
-// const authRouter = require('./routes/auth');
-// const subjectRouter = require('./routes/subjects');
-// const taskRouter = require('./routes/tasks');
+const userRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
+const subjectRouter = require('./routes/subjects');
+const taskRouter = require('./routes/tasks');
 const PORT = 3000;
 const mongoose = require('mongoose');
 
@@ -11,7 +11,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Connect to MongoDB
-mongoose.connect("mongodb+srv://kaito:abc@cluster0.hy7ef.mongodb.net/threadBoard?retryWrites=true&w=majority&appName=Cluster0")
+//.envファイルに記述したURIを読み込む
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
     .then(() =>
         console.log("888888888888888888888888888888")
     ).catch((err) =>
@@ -22,20 +26,21 @@ mongoose.connect("mongodb+srv://kaito:abc@cluster0.hy7ef.mongodb.net/threadBoard
 app.use(express.json());
 
 // Routesの読み込み　try-catchでエラーをキャッチ
-// try{
-//     app.use('/users', userRouter);
-//     app.use('/auth', authRouter);
-//     app.use('/subjects', subjectRouter);
-//     app.use('/tasks', taskRouter);    
-// } catch (err) {
-//     console.log("ルートの読み込みエラー", err.message);
-// }
+try{
+    app.use('/users', userRouter);
+    app.use('/auth', authRouter);
+    app.use('/subjects', subjectRouter);
+    app.use('/tasks', taskRouter);    
+} catch (err) {
+    console.log("ルートの読み込みエラー", err.message);
+}
 
-//サーバテスト用
+/*
+//テスト用
 app.get('/', (req, res) => {
-    res.send("サーバーは正常に動作しています");
+    res.send("ねむいっす");
 });
-
+*/
 
 // Serverを起動
 app.listen(PORT, () => console.log("server is running on PORT" + PORT));
