bbc0dac47130f474f4e0072ee9c4af428e7badd6
小野瑛人 <eito@Eitos-MacBook-Air.local>
2025-02-01 17:20:18 +0900

    WIP: 作業中の変更をコミット

--- Study_Manager/src/Level.js
+++ Study_Manager/src/Level.js
@@ -1,2 +1,13 @@
 import SubjectList from "./SubjectList";
 
+function Level({ subjects, addTask }) {
+    const totalXP = subjects.reduce((acc, subject) => acc + subject.XP, 0);
+    const level = Math.floor(totalXP / xpPerLevel) + 1;
+
+    return (
+        <div>
+            <h1>レベル {level}</h1>
+            <SubjectList subjects={subjects} addTask={addTask} />
+        </div>
+    );
+}
